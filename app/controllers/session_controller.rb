7ea2fcd780b75200453634691150f93887355c9d
QuickResolve <lauranmontoya@gmail.com>
2016-04-26 22:36:54 -0700

    set up sessions controller

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,28 +1,35 @@
 #create new session page
 get '/login' do
 	erb :'/sessions/session_new'
+	# if request.xhr?
+	# 	erb :'/sessions/_new_session_form', layout: false
+	# else
+	# erb :'/sessions/session_new'
+	# end
 end
-
 
 #login to session
 post '/sessions' do
-	user = User.find_by(email: params[:email])
-	if user && user.password = params[:password]
-		session[:id] = user.id
-		redirect '/'
-	else
-		# flash[:errors] = ['Incorrect Username or Password']
-		status 400
-		redirect '/login'
-	end
+  access = User.authenticate(params[:email], params[:password])
+  if access
+    user_id = User.find_by(email: params[:email]).id
+    puts "This is the user id:" + user_id.to_s
+    login(user_id)
+    set_login_success_message
+    puts session[:user_id]
+    redirect "/users/#{session[:user_id]}"
+  else
+    set_error_message
+    redirect '/'
+  end
 end
 
 
 #delete session
-delete '/sessions/:id' do
-	current_user = nil
-	session[:id] = nil
-	redirect '/templates'
+get '/logout' do
+  logout!
+  set_logout_success_message
+  redirect '/'
 end
 
 
