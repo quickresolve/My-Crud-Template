b9a05c6adde8496963844ba12c5bbae75cb395b2
QuickResolve <lauranmontoya@gmail.com>
2016-04-26 22:55:12 -0700

    update user controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -7,36 +7,48 @@ end
 #new user form
 get '/users/new' do
 	erb :'/users/user_new'
+	# if request.xhr?
+	# 	erb :'/users/_new_user_form', layout: false
+	# else
+	# 	erb :'/users/user_new'
+	# end
 end
 
 #create new user
 post '/users' do
-	user = User.new(username: params[:username], email: params[:email])
-	user.password = params[:password]
-	if user.save
-		session[:id] = user.id
-		redirect '/'
-	else
-		# status 400
+  new_user = User.new(username: params[:username], email: params[:email] , password: params[:password])
+  if new_user.valid?
+  	new_user.save
+  	login(new_user)
+  	new_user_success_message
+  	redirect "/"
+  else
+  	# status 400
 		# flash[:errors] = user.errors.full_messages
 		redirect '/users/new'
-	end
-end
-
-#get edit page
-get '/users/:id/edit' do
-	@user = User.find(params[:id])
-	erb :'/users/user_edit'
+  end
 end
 
 #show a user
 get '/users/:id' do
-	@user = User.find(params[:id])
+	 @user = current_user
+	 erb :'users/user_show'
+end
+
+#get edit page
+get '/users/:id/edit' do
+	@user = current_user
+	erb :'/users/user_edit'
+	# if request.xhr?
+	# 	erb :'/users/_edit_user_form', layout: false
+	# else
+	# 	erb :'/users/user_edit'
+	# end
 end
 
 #submit user edit
 put '/users/:id' do
-	user = User.find(params[:id])
+	user = current_user
 	user.username = params[:username]
 	user.email = params[:email]
 	if user.save
@@ -49,9 +61,10 @@ end
 
 #delete user
 delete '/users/:id' do
-	User.find(params[:id]).destroy
-	session[:id] = nil
-	current_user = nil
+  user = current_user
+  user.destroy
+  logout!
+  redirect '/'
 end
 
 
